90df59337f4c610fb6c8e08840e26bb34d0f8c6d
Guilherme Eduardo <gededeus@gmail.com>
2025-12-07 18:55:43 -0300

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+void printCalculation(float value1, float value2, char symbol) {
+
+    switch (symbol) {
+    case '+':
+        std::cout << "The result is: " << value1 + value2 << '\n';
+        return; 
+
+    case '-':
+        std::cout << "The result is: " << value1 - value2 << '\n';
+        return;
+
+    case '*':
+        std::cout << "The result is: " << value1 * value2 << '\n';
+        return;
+
+    case '/':
+        if (value2 == 0.0) {
+            std::cout << "Error: Division by zero!\n";
+            return;
+        }
+
+        std::cout << "The result is: " << value1 / value2 << '\n';
+        return;
+
+    default:
+        std::cout << "Error: Invalid operator or value!\n";
+        return;
+    }
+}
+
+int main()
+{
+    float value1{ 0 }, value2{ 0 };
+    char symbol;
+
+    std::cout << "Enter two values:\n";
+    std::cin >> value1 >> value2;
+
+    std::cout << "Enter an operator:\n";
+    std::cin >> symbol;
+
+    printCalculation(value1, value2, symbol);
+
+    return 0;
+}
